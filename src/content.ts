2dd9add563af8e934f7c95204eeb4687a3f93687
Lewis Peterson <lewispeterson@gmail.com>
2026-04-01 14:19:18 -0700

    Text tweaks

--- src/content.ts
+++ src/content.ts
@@ -1,16 +1,16 @@
 export const content = {
   global: {
-    appTitle: "[ APT // Apocalypse Personal Trainer ]",
+    appTitle: "[ APT // Your Apocalypse Personal Trainer ]",
     footer: "APT v1.0 // Hosted locally // Stay safe out there"
   },
   landing: {
     subtitle: "> Connection established...",
-    welcomeHeader: "Juice here (RIP Toya)",
-    welcomeBody: "Hey fam, it's Juice. My girl Toya is with the angels but I found a gym with a working server so I'm BACK. Time to get ripped so we can flip the script on those bony-assed bots. So find some water, shoes and grab some scrap metal and let's do this.  Create an account and choose 2 Week track:",
+    welcomeHeader: "Your Trainer Juice is Back!! (RIP Toya)",
+    welcomeBody: "Hey fam, it's Juice. My girl Toya is with the angels but I found a gym with a working server so I'm BACK. Time to get ripped so we can flip the script on those bony-assed bots. So find some water, shoes, grab some scrap metal and let's do this.  To get started, create an account and choose one of the 2 Week tracks:",
     welcomeList: [
-      "Fitness Track: Scatter, Scavenge, Look Amazing",
-      "Wellness Track: Tune ur Gut and Mental",
-      "Combat Track: David v. Goliath for Real"
+      "Fitness Track   (Scatter, Scavenge, Look Amazing)",
+      "Wellness Track  (Tune ur Gut and Harden ur Mental)",
+      "Combat Track    (David v. Goliath for Real)"
     ],
     btnLogin: "[ Log In ]",
     btnDashboard: "[ Go to Dashboard ]"
